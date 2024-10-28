c1908ed8d7f740ef8d01d8696ad2da4a061fa13d
L4UR039 <110909051+L4UR039@users.noreply.github.com>
2024-10-28 17:03:12 -0300

    Create fundamentos07.js

---
+++ fundamentos/fundamentos07.js
@@ -0,0 +1,12 @@
+let frase = 'frase'
+let tamanho = frase.length
+let maiusculo = frase.toLocaleUpperCase
+let minusculo = frase.toLocaleLowerCase
+
+if (tamanho < 5){
+  console.log(maiusculo)
+}
+
+else{
+  console.log(minusculo)
+}
