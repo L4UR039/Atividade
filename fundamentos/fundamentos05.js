71e3195f6a9a26e3c9a4aed6356d31ba6d80c9e8
L4UR039 <110909051+L4UR039@users.noreply.github.com>
2024-10-28 16:36:23 -0300

    Create fundamentos05.js

---
+++ fundamentos/fundamentos05.js
@@ -0,0 +1,14 @@
+let number1 = 39
+let number2 = 12
+let number3 = 45
+
+if (number1 & number2 & number3 < 50){
+  console.log ('ERROR')
+}
+if (number1 & number2 & number3 > 90){
+  console.log ('ERROR')
+}
+
+else {
+  console.log ('OK')
+}
